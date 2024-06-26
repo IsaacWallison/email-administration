f5e57ed6ea7f173c68635f0fb9be2b1eaf613b42
Isaac <isaacwls@gmail.com>
2024-06-25 22:04:42 -0300

    update app.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Email email = new Email("harry", "potter");
-        var info = email.showInfo();
-        System.out.println(info);
+        Email email = new Email("test", "test");
+        System.out.println(email.showInfo());
     }
 }
